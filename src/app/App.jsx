028cf0578b9a154a1fc305abe1a3a5f3f747484a
TheoCRLDS <theo.carladous@laposte.net>
2023-07-17 10:09:31 +0200

    react-router-dom

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -5,16 +5,17 @@ import Register from "../Components/Auth/Register/Register";
 import Login from "../Components/Auth/Login/Login";
 
 function App() {
-  return;
-  <div>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
-  </div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
