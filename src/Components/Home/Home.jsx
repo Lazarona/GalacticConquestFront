fda24335d60e37c66b5405ad1900efc151acc014
TheoCRLDS <theo.carladous@laposte.net>
2023-07-17 12:11:59 +0200

    logo shortcut icon

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,15 +3,13 @@ import "./Home.css";
 function Home() {
   return (
     <>
-    <div className="logo">
-      <img src="src/Components/img/logo.png" alt="" />
-    </div>
+      <div className="logo">
+        <img src="src/Components/img/logo.png" alt="" />
+      </div>
 
-    <div>
-      
-
-    </div>
-    
+      <div>
+        <h1>Galactic Conquest</h1>
+      </div>
     </>
   );
 }
