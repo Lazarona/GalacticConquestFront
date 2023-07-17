9fe29f38aa0fed2268a8bfe350b383c8d2e93a35
Celine CC <celine.c11@outlook.com>
2023-07-17 14:13:20 +0200

    LienpageHome

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,21 +1,34 @@
 import "./Home.css";
 import React from 'react';
 import { MDBBtn } from 'mdb-react-ui-kit';
+import { useNavigate } from "react-router-dom";
 
 function Home() {
+
+  const navigate = useNavigate();
+
+  const navLogin = () => {
+    navigate("/login");
+  }
+  const navRegister = () => {
+    navigate("/register");
+  }
+
+
+
   return (
-    <>
+    <div id="home-container">
       <div className="logo">
         <img src="src/Components/img/logo.png" alt="" />
       </div>
 
-    <div className="btn">
-      <MDBBtn>Créer un compte</MDBBtn>
-      <MDBBtn>Connexion</MDBBtn>
+    <div className="allbouton">
+      <MDBBtn className="bouton" onClick={navRegister}>Créer un compte</MDBBtn>
+      <MDBBtn className="bouton"onClick={navLogin} >Connexion</MDBBtn>
 
     </div>
     
-    </>
+    </div>
   );
 }
 
