2668aa70a938925a02ff9dbf78370d78d700e278
TheoCRLDS <theo.carladous@laposte.net>
2023-07-17 10:03:11 +0200

    first commit

---
+++ src/app/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "../Components/Home/Home";
+import Register from "../Components/Auth/Register/Register";
+import Login from "../Components/Auth/Login/Login";
+
+function App() {
+  return;
+  <div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  </div>;
+}
+
+export default App;
