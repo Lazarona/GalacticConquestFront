d3ff7be239bf82ae05c8de3bfe0834f3c8f5c545
TheoCRLDS <theo.carladous@laposte.net>
2023-07-19 15:14:49 +0200

    useContext Auth with all logged user info

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -3,17 +3,24 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../Components/Home/Home";
 import Register from "../Components/Auth/Register/Register";
 import Login from "../Components/Auth/Login/Login";
+import { createContext, useState } from "react";
+
+export const AuthContext = createContext();
 
 function App() {
+  const [userLogged, setUserLogged] = useState(null);
+
   return (
     <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </BrowserRouter>
+      <AuthContext.Provider value={[userLogged, setUserLogged]}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </BrowserRouter>
+      </AuthContext.Provider>
     </div>
   );
 }
