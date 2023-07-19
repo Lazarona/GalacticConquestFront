7d978445c450403a7ddf651069345b58d1853b16
TheoCRLDS <theo.carladous@laposte.net>
2023-07-19 16:49:25 +0200

    PlanetPseudo component methods

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -1,3 +1,19 @@
-function Dashboard() {}
+import "./Dashboard.css";
+import { useNavigate } from "react-router-dom";
+import React, { useContext, useEffect, useState } from "react";
+import { MDBInput, MDBBtn } from "mdb-react-ui-kit";
+import { AuthContext } from "../../app/App";
+
+function Dashboard() {
+  const [userLogged, setUserLogged] = useContext(AuthContext);
+
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <h1>Dashboard</h1>
+    </div>
+  );
+}
 
 export default Dashboard;
