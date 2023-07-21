0e4340f92697f176584a89c16c59bd7f05412e23
Celine CC <celine.c11@outlook.com>
2023-07-21 09:24:42 +0200

    dashboard

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -1,8 +1,16 @@
 import "./Dashboard.css";
 import { useNavigate } from "react-router-dom";
 import React, { useContext, useEffect, useState } from "react";
-import { MDBInput, MDBBtn } from "mdb-react-ui-kit";
+import {
+  MDBTooltip,
+  //   MDBInput,
+  //   MDBBtn,
+  MDBPopover,
+  MDBPopoverBody,
+  MDBPopoverHeader,
+} from "mdb-react-ui-kit";
 import { AuthContext } from "../../app/App";
+// import { url } from "inspector";
 
 function Dashboard() {
   const [userLogged, setUserLogged] = useContext(AuthContext);
@@ -14,109 +22,178 @@ function Dashboard() {
   };
 
   return (
-    <div id="dashboard-container">
-      <img
-        className="logonav mt-3 ms-3"
-        type="button"
-        data-bs-toggle="offcanvas"
-        data-bs-target="#offcanvasScrolling"
-        aria-controls="offcanvasScrolling"
-        src="src/Components/img/nvb.png"
-        alt=""
-      />
-      <div
-        className="offcanvas offcanvas-start w-25 p-3 bg-black"
-        data-bs-scroll="true"
-        data-bs-backdrop="false"
-        tabIndex="-1"
-        id="offcanvasScrolling"
-        aria-labelledby="offcanvasScrollingLabel"
-      >
-        <div className="offcanvas-header">
-          <h5 className="offcanvas-title" id="offcanvasScrollingLabel">
-            MENU
-          </h5>
+    <>
+      <div id="dashboard-container">
+        <img
+          className="logonav mt-3 ms-3"
+          type="button"
+          data-bs-toggle="offcanvas"
+          data-bs-target="#offcanvasScrolling"
+          aria-controls="offcanvasScrolling"
+          src="src/Components/img/nvb.png"
+          alt=""
+        />
+        <div
+          className="offcanvas offcanvas-start w-25 p-3 navbarmenu"
+          data-bs-scroll="true"
+          data-bs-backdrop="false"
+          tabIndex="-1"
+          id="offcanvasScrolling"
+          aria-labelledby="offcanvasScrollingLabel"
+        >
+          <div className="offcanvas-header">
+            <h5 className="offcanvas-title" id="offcanvasScrollingLabel"></h5>
+            <button
+              type="button"
+              className="boutonclose btn-close "
+              data-bs-dismiss="offcanvas"
+              aria-label="FERMER"
+            ></button>
+          </div>
+          <div className="offcanvas-body d-flex flex-column mb-3 gap-3">
+            <a className="boutonm">PROFIL</a>
+            <a className="boutonm">ACHAT</a>
+            <a className="boutondec" onClick={navHome}>
+              DECONNEXION
+            </a>
+          </div>
+        </div>
+
+        <div className="dropdown vaisseaux">
           <button
+            className="btn btn-secondary dropdown-toggle boutonnav"
             type="button"
-            className="boutonclose btn-close "
-            data-bs-dismiss="offcanvas"
-            aria-label="FERMER"
-          ></button>
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            Vaisseaux
+          </button>
+          <ul className=" allv dropdown-menu">
+            <li>
+              <a className=" nom dropdown-item" href="#">
+                Chasseur: 45
+              </a>
+            </li>
+            <li>
+              <a className="nom dropdown-item" href="#">
+                Fregate:
+              </a>
+            </li>
+            <li>
+              <a className="nom dropdown-item" href="#">
+                Croisseur:
+              </a>
+            </li>
+            <li>
+              <a className="nom dropdown-item" href="#">
+                Destroyeur:
+              </a>
+            </li>
+          </ul>
         </div>
-        <div className="offcanvas-body d-flex flex-column mb-3 gap-3">
-          <a className="boutonm">PROFIL</a>
-          <a className="boutonm">ACHAT</a>
-          <a className="boutondec" onClick={navHome}>
-            DECONNEXION
-          </a>
+        <div className="energie">
+          <img
+            className="icons align-self-center"
+            src="src/Components/img/icone-ressource-minerai.png"
+            alt=""
+          />
+          <h2> 30000</h2>
+        </div>
+        <div className="energie">
+          <img
+            className="icons align-self-center"
+            src="src/Components/img/icone-ressource-carburant.png"
+            alt=""
+          />
+          <h2> 1000</h2>
+        </div>
+        <div className="energie">
+          <h2> ⚡️ 45</h2>
         </div>
       </div>
-      <div class="dropdown">
-        <button
-          class="btn btn-secondary dropdown-toggle"
-          type="button"
-          data-bs-toggle="dropdown"
-          aria-expanded="false"
+      <div>
+        <MDBPopover
+          btnClassName="planete"
+          className="popinfra "
+          placement="right"
         >
-          Entrepôt
-        </button>
-        <ul class="dropdown-menu">
-          <li>
-            <a class="dropdown-item" href="#">
-              <img
-                className="icons"
-                src="src/Components/img/icone-ressource-minerai.png"
-                alt=""
-              />
-            </a>
-          </li>
-          <li>
-            <a class="dropdown-item" href="#">
-              <img
-                className="icons"
-                src="src/Components/img/icone-ressource-carburant.png"
-                alt=""
-              />
-            </a>
-          </li>
-        </ul>
-      </div>
-      <div class="dropdown">
-        <button
-          class="btn btn-secondary dropdown-toggle"
-          type="button"
-          data-bs-toggle="dropdown"
-          aria-expanded="false"
+          <MDBPopoverHeader className="titleinfra">
+            <div className="d-flex justify-content-center align-items-center">
+              <p className="infra">INFRASTRUCTURES</p>
+            </div>
+          </MDBPopoverHeader>
+          <MDBPopoverBody className="">
+            <div className="d-flex flex-column">
+              <div className="displayiconnes d-flex justify-content-center">
+                <img
+                  className="iconnes"
+                  src="src/Components/img/icone-infrastructure-mine.png"
+                />
+                <img
+                  className="iconnes"
+                  src="src/Components/img/icone-infrastructure-centrale.png"
+                />
+                <img
+                  className="iconnes"
+                  src="src/Components/img/icone-infrastructure-raffinerie.png"
+                />
+              </div>
+              <div className=" displayiconnes d-flex justify-content-center ">
+                <a className="construire" href="">
+                  Construire
+                </a>
+                <a className="construire" href="">
+                  Construire
+                </a>
+                <a className="construire" href="">
+                  Construire
+                </a>
+              </div>
+              <div className=" d-flex justify-content-center mt-3">
+                <button className="myinfra ">Mes Infrastructures</button>
+              </div>
+            </div>
+          </MDBPopoverBody>
+        </MDBPopover>
+        <MDBPopover
+          btnClassName="planete2"
+          className="popinfra2 "
+          placement="right"
         >
-          Vaisseaux
-        </button>
-        <ul class="dropdown-menu">
-          <li>
-            <a class="dropdown-item" href="#">
-              Chasseur:
-            </a>
-          </li>
-          <li>
-            <a class="dropdown-item" href="#">
-              Fregate:
-            </a>
-          </li>
-          <li>
-            <a class="dropdown-item" href="#">
-              Croisseur:
-            </a>
-          </li>
-          <li>
-            <a class="dropdown-item" href="#">
-              Destroyeur:
-            </a>
-          </li>
-        </ul>
+          <MDBPopoverHeader className="titleinfra2">
+            <div className="d-flex justify-content-center align-items-center">
+              <p className="infra2">CHANTIER SPACIAL</p>
+            </div>
+          </MDBPopoverHeader>
+          <MDBPopoverBody className="">
+            <div className="d-flex flex-column">
+              <div className="displayiconnes2 d-flex justify-content-center">
+                <img
+                  className="iconnes2"
+                  src="src/Components/img/icone-vaisseau-destroyer.png"
+                />
+              </div>
+              <div className=" displayiconnes2 d-flex justify-content-center ">
+                <a className="construire2" href="">
+                  Construire
+                </a>
+              </div>
+              <div className=" d-flex justify-content-center mt-3">
+                <button className="myinfra2 ">Mon Chantier Spacial</button>
+              </div>
+            </div>
+          </MDBPopoverBody>
+        </MDBPopover>
       </div>
-      <div className="energie">
-        <h4>⚡️</h4>
-      </div>
-    </div>
+
+      {/* 
+      <MDBPopover btnClassName="imgPlanet" btnChildren="" placement="right">
+        <MDBPopoverHeader>Popover title</MDBPopoverHeader>
+        <MDBPopoverBody>
+          And here's some amazing content. It's very engaging. Right?
+        </MDBPopoverBody>
+      </MDBPopover> */}
+    </>
   );
 }
 
