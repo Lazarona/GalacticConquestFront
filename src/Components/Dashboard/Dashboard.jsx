c6d7360a89f84c22e31edad46ab55eccf0d7d6c3
Celine CC <celine.c11@outlook.com>
2023-07-23 08:53:57 +0200

    details dash-infra-planetp

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -338,13 +338,13 @@ function Dashboard() {
               <h2> {displayEnergy()}</h2>
             </div>
           </div>
-          <a href="" className="attaquecontainer me-3 mt-2">
+          <div className="attaquecontainer me-3 mt-2">
             <img
               className="attaque"
               src="src/Components/img/Attaque.png"
               alt=""
             />
-          </a>
+          </div>
           <div>
             <MDBPopover
               btnClassName="planete"
