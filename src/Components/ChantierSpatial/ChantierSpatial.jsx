da061118d786a921b64c02a8cbf140b0eed94eff
Celine CC <celine.c11@outlook.com>
2023-07-25 14:42:22 +0200

    navbar sur page chantier spatial

--- src/Components/ChantierSpatial/ChantierSpatial.jsx
+++ src/Components/ChantierSpatial/ChantierSpatial.jsx
@@ -11,6 +11,8 @@ function ChantierSpatial() {
   const [frigate, setFrigate] = useState([]);
   const [cruiser, setCruiser] = useState([]);
   const [destroyer, setDestroyer] = useState([]);
+  const [name, setName] = useState([]);
+  const [username, setUsername] = useState([]);
 
   const navigate = useNavigate();
 
@@ -223,15 +225,78 @@ function ChantierSpatial() {
     }
   };
 
+  // Affichage du nom de la planete
+
+  const planetName = async () => {
+    const options = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    };
+    const response = await fetch("http://127.0.0.1:8000/api/planet", options);
+    const donnees = await response.json();
+    console.log("Reponse de l'API (Planet): ", donnees);
+    setName(donnees.planet);
+  };
+
+  const displayPlanet = () => {
+    return name.map((e, index) => {
+      return (
+        <div key={index}>
+          <p className="nameplanet">{e.name}</p>
+        </div>
+      );
+    });
+  };
+
+  // Affichage du nom du username
+
+  const identifiant = async () => {
+    const options = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    };
+    const response = await fetch("http://127.0.0.1:8000/api/user", options);
+    const donnees = await response.json();
+    console.log("Reponse de l'API (username): ", donnees);
+    setUsername(donnees.user);
+  };
+
+  const displayUsername = () => {
+    return username.map((e, index) => {
+      return (
+        <div key={index}>
+          <p className="username ">{e.username}</p>
+        </div>
+      );
+    });
+  };
   useEffect(() => {
     getShipyards();
+    planetName();
+    identifiant();
   }, []);
+
   useEffect(() => {
     console.log("Shipyards : ", shipyards);
   }, [shipyards, setShipyards]);
   // useEffect(() => {
   //   console.log("Hunter : ", hunter);
   // }, [hunter, setHunter]);
+  useEffect(() => {
+    console.log("Planet : ", name);
+  }, [setName, name]);
+
+  useEffect(() => {
+    console.log("Username : ", username);
+  }, [setUsername, username]);
 
   return (
     <>
@@ -247,53 +312,71 @@ function ChantierSpatial() {
           </div>
         </div>
       ) : (
-        <div id="cs-container">
-          <div className="navbar d-flex ">
-            <img
-              className="logonav mt-2 ms-3"
-              type="button"
-              data-bs-toggle="offcanvas"
-              data-bs-target="#offcanvasScrolling"
-              aria-controls="offcanvasScrolling"
-              src="src/Components/img/nvb.png"
-              alt=""
-            />
-            <div
-              className="offcanvas offcanvas-start p-3 navbarmenu"
-              data-bs-scroll="true"
-              data-bs-backdrop="false"
-              tabIndex="-1"
-              id="offcanvasScrolling"
-              aria-labelledby="offcanvasScrollingLabel"
-            >
-              <div className="offcanvas-header">
-                <h5
-                  className="offcanvas-title"
-                  id="offcanvasScrollingLabel"
-                ></h5>
-                <button
-                  type="button"
-                  className="boutonclose btn-close "
-                  data-bs-dismiss="offcanvas"
-                  aria-label="FERMER"
-                ></button>
-              </div>
-              <div className="offcanvas-body d-flex flex-column mb-3 gap-3">
-                <a className="boutonm">PROFIL</a>
-                <a className="boutonm">ACHAT</a>
-                <a className="boutondec" onClick={navHome}>
-                  DECONNEXION
-                </a>
-              </div>
-            </div>
+        <div id="chantierspatial">
+          <div id="cs-container">
+            <div className=" d-flex justify-content-between align-items-center">
+              <img
+                className="logonav ms-3 "
+                type="button"
+                data-bs-toggle="offcanvas"
+                data-bs-target="#offcanvasScrolling"
+                aria-controls="offcanvasScrolling"
+                src="src/Components/img/nvb.png"
+                alt=""
+              />
 
-            <h2>CHANTIER SPATIAL</h2>
-            <img
-              className="return me-1"
-              src="src/Components/img/flecheRetour.png"
-              alt=""
-              onClick={navDashboard}
-            />
+              <div
+                className="offcanvas offcanvas-start w-25 p-3 navbarmenu"
+                data-bs-scroll="true"
+                data-bs-backdrop="false"
+                tabIndex="-1"
+                id="offcanvasScrolling"
+                aria-labelledby="offcanvasScrollingLabel"
+              >
+                <div className="offcanvas-header">
+                  <h5
+                    className="offcanvas-title"
+                    id="offcanvasScrollingLabel"
+                  ></h5>
+                  <button
+                    type="button"
+                    className="boutonclose btn-close "
+                    data-bs-dismiss="offcanvas"
+                    aria-label="FERMER"
+                  ></button>
+                </div>
+                <div className="offcanvas-body d-flex flex-column mb-3 gap-3">
+                  <div className="contenuusername d-flex align-items-center ">
+                    <img
+                      className="logousername"
+                      src="src/Components/img/logousername.png"
+                      alt=""
+                    />
+                    {displayUsername()}
+                    <div className="d-flex align-items-baseline gap-2  ">
+                      <img
+                        className="imgplanetusername"
+                        src="src/Components/img/planet1.png"
+                        alt=""
+                      />
+                      {displayPlanet()}
+                    </div>
+                  </div>
+                  <a className="boutonm pt-3">ACHAT</a>
+                  <a className="boutondec" onClick={deconnexion}>
+                    DECONNEXION
+                  </a>
+                </div>
+              </div>
+
+              <h2>CHANTIER SPATIAL</h2>
+              <img
+                className="return me-1"
+                src="src/Components/img/flecheRetour.png"
+                alt=""
+                onClick={navDashboard}
+              />
+            </div>
           </div>
 
           {/* ---------BODY------ */}
