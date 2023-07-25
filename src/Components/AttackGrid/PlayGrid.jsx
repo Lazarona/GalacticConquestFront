3ecdaeae8ab46fcc7af23582209a9e117dfd5911
Lazarone <leabarisani@gmail.com>
2023-07-25 15:52:02 +0200

    gridModifEssaieCoeDedeplacement

--- src/Components/AttackGrid/PlayGrid.jsx
+++ src/Components/AttackGrid/PlayGrid.jsx
@@ -1,43 +1,37 @@
 import "./PlayGrid.css";
 import React, { useState } from "react";
 
+// const construction = async () => {
+//   const options = {
+//     method: "GET",
+//     headers: {
+//       "Content-Type": "application/json",
+//       Accept: "application/json",
+//       Authorization: `Bearer ${localStorage.getItem("token")}`,
+//     },
+//   };
+//   const response = await fetch("http://127.0.0.1:8000/api/position", options);
+//   const data = await response.json();
+//   console.log("Reponse de l'API : ", data);
+//   setReponse(data);
+// };
+
+//onnées reçues luent grâce à JSON
+
 const PlayGrid = () => {
-  const gridSize = 10;
-  const fuelConso = {
-    Chasseur: 1,
-    Fregate: 2,
-    Croiseur: 4,
-    Destroyer: 8,
-  };
-  const retour = () => {
+  const dashboard = () => {
     navigate("/dashboard");
   };
-  const [selectedTarget, setSelectedTarget] = useState(null);
-  const [carburant, setCarburant] = useState(10); // Carburant initial du joueur (vous pouvez ajuster la valeur)
 
-  // Fonction pour envoyer les vaisseaux vers la cible sélectionnée
-  const sendShips = (vaisseaux) => {
-    const carburantNecessaire = vaisseaux.reduce(
-      (total, vaisseau) => total + fuelConso[vaisseau.type] * vaisseau.quantite,
-      0
-    );
-    if (carburant >= carburantNecessaire) {
-      // Envoi des vaisseaux ici (vous pouvez ajouter la logique de gestion des vaisseaux en campagne)
-      setCarburant(carburant - carburantNecessaire);
-      alert("Vaisseaux envoyés avec succès !");
-    } else {
-      alert(
-        "Vous n'avez pas assez de carburant pour effectuer ce déplacement."
-      );
-    }
-  };
+  const gridSize = 10;
+  const [selectedTarget, setSelectedTarget] = useState(null);
 
   // Fonction pour sélectionner une cible
   const selectTarget = (target) => {
     setSelectedTarget(target);
   };
 
-  // Fonction pour produire plus de carburant (non implémentée ici, vous pouvez le faire selon vos besoins)
+  // // Fonction pour produire plus de carburant (non implémentée ici, vous pouvez le faire selon vos besoins)
 
   const renderGrid = () => {
     const grid = [];
@@ -47,6 +41,7 @@ const PlayGrid = () => {
           selectedTarget && selectedTarget.x === x && selectedTarget.y === y
             ? "cell target"
             : "cell";
+
         grid.push(
           <div
             key={`${x}-${y}`}
@@ -65,35 +60,58 @@ const PlayGrid = () => {
   return (
     <div id="play-gird">
       <div className="d-flex flex-row-reverse">
-        <button className="goback" onClick={retour}>
-          RETOUR
-        </button>
+        <img
+          className="returnptodash"
+          src="src/Components/img/flecheRetour.png"
+          alt=""
+          onClick={dashboard}
+        />
       </div>
-      <div>
-        <h1>Bataille Navale - Galaxie</h1>
-        <div className="grid">{renderGrid()}</div>
-        {selectedTarget && (
-          <div className="controls">
-            <h2>
-              Cible sélectionnée : {`${selectedTarget.x},${selectedTarget.y}`}
-            </h2>
-            <p>Carburant disponible : {carburant}</p>
-            <p>
-              Carburant nécessaire pour déplacement : TODO (calculer selon les
-              vaisseaux sélectionnés)
-            </p>
-            <div className="d-flex justify-content-center">
-              <button
-                className="sendsy"
-                onClick={() =>
-                  sendShips(/* TODO: passer les vaisseaux sélectionnés ici */)
-                }
-              >
-                Envoyer les vaisseaux
-              </button>
+      <h1>Bataille Spatiale - Galaxie</h1>
+
+      <div className="row">
+        <div className="col-sm-6">
+          <div> </div>
+
+          <div className="card-histo-attac" class="">
+            <div className="card-body">
+              <h5 className="card-title">Historique de combat</h5>
+              <p className="card-text">
+                TEST d'apparition d'historique de combat
+              </p>
+              <a href="#" className="btn btn-primary">
+                Masquer l'Historique
+              </a>
             </div>
           </div>
-        )}
+        </div>
+        <div className="col-sm-6">
+          <div className="card-attack">
+            <div className="card-body">
+              <h5 className="card-title">Combat</h5>
+              <p className="card-text">
+                <div>
+                  <div className="grid">{renderGrid()}</div>
+                  {selectedTarget && (
+                    <div className="controls">
+                      <h2>
+                        Cible sélectionnée :{" "}
+                        {`${selectedTarget.x},${selectedTarget.y}`}
+                      </h2>
+                      <p>Carburant disponible : en test</p>
+                      <p></p>
+                    </div>
+                  )}
+                </div>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="d-flex justify-content-center">
+        <button className="sendsy" onClick={() => sendShips()}>
+          Envoyer les vaisseaux
+        </button>
       </div>
     </div>
   );
