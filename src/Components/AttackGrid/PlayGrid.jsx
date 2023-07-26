94c1b061b2079c56d47bbfc3ceffccd1667dea72
TheoCRLDS <theo.carladous@laposte.net>
2023-07-26 14:09:33 +0200

    function battle

--- src/Components/AttackGrid/PlayGrid.jsx
+++ src/Components/AttackGrid/PlayGrid.jsx
@@ -1,26 +1,32 @@
 import { useNavigate } from "react-router-dom";
 import "./PlayGrid.css";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const PlayGrid = () => {
   const [selectedTarget, setSelectedTarget] = useState(null);
-  const [position, setPosition] = useState(null);
+  const [positions, setPositions] = useState(null);
   const [battle, setBattle] = useState(null);
 
   const gridSize = 10;
 
-  const navigate = useNavigate;
+  const navigate = useNavigate();
 
-  const dashboard = () => {
+  const navDashboard = () => {
     navigate("/dashboard");
+  };
+
+  const deconnexion = () => {
+    localStorage.removeItem("token");
+    navigate("/");
   };
 
   // Fonction pour sélectionner une cible
   const selectTarget = (target) => {
     setSelectedTarget(target);
+    getBattle();
   };
 
-  const getPosition = async () => {
+  const getPositions = async () => {
     const options = {
       method: "GET",
       headers: {
@@ -29,10 +35,13 @@ const PlayGrid = () => {
         Authorization: `Bearer ${localStorage.getItem("token")}`,
       },
     };
-    const response = await fetch("http://127.0.0.1:8000/api/position", options);
+    const response = await fetch(
+      "http://127.0.0.1:8000/api/positions",
+      options
+    );
     const data = await response.json();
-    console.log("Reponse de l'API (Position): ", data);
-    setPosition(data);
+    console.log("Reponse de l'API (Positions): ", data);
+    setPositions(data.planets);
   };
 
   const getBattle = async () => {
@@ -45,7 +54,7 @@ const PlayGrid = () => {
       },
     };
     const response = await fetch(
-      "http://127.0.0.1:8000/api/attack/{defenderId}",
+      `http://127.0.0.1:8000/api/attack/${null}`,
       options
     );
     const data = await response.json();
@@ -73,67 +82,112 @@ const PlayGrid = () => {
         );
       }
     }
-
     return grid;
   };
 
-  return (
-    <div id="play-gird">
-      <div className="d-flex flex-row-reverse">
-        <img
-          className="returnptodash"
-          src="src/Components/img/flecheRetour.png"
-          alt=""
-          onClick={dashboard}
-        />
-      </div>
-      <h1 className="bataille-title">Bataille Spatiale - Galaxie</h1>
+  const displayErrors = () => {
+    if (erreurs.errors == undefined && erreurs.message == undefined) {
+      return null;
+    }
+    if (erreurs.message != undefined) {
+      return Object.keys(erreurs).map((key) => {
+        return (
+          <ul key={key}>
+            <h4 className="titlecrea">{erreurs.message}</h4>
+          </ul>
+        );
+      });
+    } else {
+      return Object.keys(erreurs).map((key) => {
+        return (
+          <ul key={key}>
+            <h1>{erreurs.errors.name}</h1>
+          </ul>
+        );
+      });
+    }
+  };
 
-      <div className="row">
-        <div className="col-sm-6">
-          <div className="cardattack">
-            <div className="card-body">
-              <h5 id="card-title">Historique de combat</h5>
-              <p className="card-text">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Provident placeat numquam expedita enim veniam, fugiat
-                laudantium dolores odio iure, reiciendis quas qui quis facere
-                obcaecati aliquid doloribus autem odit excepturi!
-              </p>
-            </div>
+  useEffect(() => {
+    getPositions();
+  }, []);
+
+  useEffect(() => {
+    console.log("Positions : ", positions);
+  }, [positions, setPositions]);
+
+  return (
+    <>
+      {localStorage.getItem("token") === null ? (
+        <div id="dashboard">
+          <div className="d-flex justify-content-center titleinfra2">
+            {displayErrors()}
+          </div>
+          <div className="d-flex justify-content-center titleinfra2">
+            <button className="myinfra" onClick={deconnexion}>
+              Revenir à l'accueil
+            </button>
           </div>
         </div>
-        <div className="col-sm-6">
-          <div className="cardattack2">
-            <div className="card-body">
-              <h5 className="card-title">Combat</h5>
-              <p className="card-text">
-                <div>
-                  <div className="grid">{renderGrid()}</div>
-                  {selectedTarget && (
-                    <div className="controls">
-                      <h2>
-                        Cible sélectionnée :{" "}
-                        {`${selectedTarget.x},${selectedTarget.y}`}
-                      </h2>
-                      <p>Carburant disponible : en test</p>
-                      <p></p>
-                    </div>
-                  )}
+      ) : (
+        <div id="play-gird">
+          <div className="d-flex flex-row-reverse">
+            <img
+              className="returnptodash"
+              src="src/Components/img/flecheRetour.png"
+              alt=""
+              onClick={navDashboard}
+            />
+          </div>
+          <h1 className="bataille-title">Bataille Spatiale - Galaxie</h1>
+
+          <div className="row">
+            <div className="col-sm-6">
+              <div className="cardattack">
+                <div className="card-body">
+                  <h5 id="card-title">Historique de combat</h5>
+                  <p className="card-text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Provident placeat numquam expedita enim veniam, fugiat
+                    laudantium dolores odio iure, reiciendis quas qui quis
+                    facere obcaecati aliquid doloribus autem odit excepturi!
+                  </p>
                 </div>
-              </p>
+              </div>
+            </div>
+            <div className="col-sm-6">
+              <div className="cardattack2">
+                <div className="card-body">
+                  <h5 className="card-title">Combat</h5>
+                  <p className="card-text">
+                    <div>
+                      <div className="grid">{renderGrid()}</div>
+                      {selectedTarget && (
+                        <div className="controls">
+                          <h2>
+                            Cible sélectionnée :{" "}
+                            {`${selectedTarget.x},${selectedTarget.y}`}
+                          </h2>
+                          <p>Carburant disponible : en test</p>
+                          <p></p>
+                        </div>
+                      )}
+                    </div>
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
+          <footer>
+            <div className="d-flex justify-content-center">
+              <button className="sendsy" onClick={() => sendShips()}>
+                Envoyer les vaisseaux
+              </button>
+            </div>
+          </footer>
         </div>
-      </div>
-      <footer>
-        <div className="d-flex justify-content-center">
-          <button className="sendsy" onClick={() => sendShips()}>
-            Envoyer les vaisseaux
-          </button>
-        </div>
-      </footer>
-    </div>
+      )}
+    </>
   );
 };
 
