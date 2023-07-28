3d2d66d682ff7fa270f7294b89d20b19582a1cbc
NicolasDEMR <nicolas.denamurr@gmail.com>
2023-07-28 09:55:47 +0200

    page de victoire

--- src/Components/Victory/Victory.jsx
+++ src/Components/Victory/Victory.jsx
@@ -2,7 +2,15 @@ import "./Victory.css";
 import React, { useState, useEffect } from "react";
 
 const OtherUserProfile = ({ userId }) => {
-  const [user, setUser] = useState(1);
+  const [user, setUser] = useState(true);
+  // État local pour stocker le nombre de ressources du joueur
+  const [ressources, setRessources] = useState({});
+
+  // État local pour indiquer si le joueur a gagné ou non
+  const [victoire, setVictoire] = useState(false);
+
+  // Pourcentage des ressources à récupérer en cas de victoire
+  const pourcentageRecupere = 0.1;
 
   useEffect(() => {
     // récupérer les informations de l'utilisateur spécifié par l'ID
@@ -42,70 +50,127 @@ const OtherUserProfile = ({ userId }) => {
   if (!user) {
     return <div className="Victory">Chargement en cours...</div>;
   }
+  useEffect(() => {
+    console.log("test", ressources);
+  }, [ressources]);
 
-  const RessourcesManager = () => {
-    // État local pour stocker le nombre de ressources du joueur
-    const [ressources, setRessources] = useState();
-
-    // État local pour indiquer si le joueur a gagné ou non
-    const [victoire, setVictoire] = useState(false);
-
-    // Pourcentage des ressources à récupérer en cas de victoire
-    const pourcentageRecupere = 0.1;
-
-    //   Recupère l'ID de l'autre planète
-    // const [id, setId] = useState();
-
-    const getResources = async () => {
-      const options = {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Accept: "application/json",
-          Authorization: `Bearer ${localStorage.getItem("token")}`,
-        },
-      };
-      const response = await fetch(
-        "http://127.0.0.1:8000/api/resource",
-        options
-      );
-      const donnees = await response.json();
-      console.log("Reponse de l'API (Resources) : ", donnees);
-      if (response.status == 401) {
-        setErreurs(donnees);
-      } else {
-        setRessources(donnees.resource);
-      }
-    };
-
-    //  clic sur le bouton "Victoire !"
-    const handleVictoire = () => {
-      setVictoire(true); // Définit l'état de victoire à true
-    };
-
-    // Fonction pour récupérer les ressources en cas de victoire
-    const recupererRessources = () => {
-      if (victoire) {
-        // Si le joueur a gagné, on calcule le nombre de ressources à récupérer
-        const ressourcesRecuperees = Math.floor(
-          ressources * pourcentageRecupere
+  const displayOres = () => {
+    if (ressources) {
+      return Object.keys(ressources).map((e, key) => {
+        return (
+          <div key={key}>
+            <p className="ore">{ressources[key].ore}</p>
+          </div>
         );
-        setRessources(ressources + ressourcesRecuperees); // Mise à jour des ressources avec celles récupérées
-      }
-    };
-
-    useEffect(() => {
-      getResources();
-    }, []);
-    return (
-      <div className="Victory">
-        <p>Ressources actuelles: {ressources}</p>
-        <button onClick={handleVictoire}>Victoire !</button>
-        <button onClick={recupererRessources}>Récupérer les ressources</button>
-      </div>
-    );
+      });
+    }
+    return <p>Loading...</p>;
   };
 
-  return <div className="Victory">{RessourcesManager()}</div>;
+  const displayFuel = () => {
+    if (ressources) {
+      return Object.keys(ressources).map((e, key) => {
+        return (
+          <div key={key}>
+            <p className="fuel">{ressources[key].fuel}</p>
+          </div>
+        );
+      });
+    }
+    return <p>Loading...</p>;
+  };
+
+  const displayEnergy = () => {
+    if (ressources) {
+      return Object.keys(ressources).map((e, key) => {
+        return (
+          <div key={key}>
+            <p className="energy">{ressources[key].energy}</p>
+          </div>
+        );
+      });
+    }
+    return <p>Loading...</p>;
+  };
+
+  //   Recupère l'ID de l'autre planète
+  // const [id, setId] = useState();
+
+  const getResources = async () => {
+    const options = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    };
+    const response = await fetch("http://127.0.0.1:8000/api/resource", options);
+    const donnees = await response.json();
+    console.log("Reponse de l'API (Resources) : ", donnees.resource);
+    if (response.status == 401) {
+      setErreurs(donnees);
+    } else {
+      setRessources(donnees.resource);
+    }
+  };
+
+  //  clic sur le bouton "Victoire !"
+  const handleVictoire = () => {
+    setVictoire(true); // Définit l'état de victoire à true
+  };
+
+  // Fonction pour récupérer les ressources en cas de victoire
+  const recupererRessources = () => {
+    if (victoire) {
+      // Si le joueur a gagné, on calcule le nombre de ressources à récupérer
+      const ressourcesRecuperees = ressources.map((item) => {
+        const { ore, fuel, energy } = item;
+        return {
+          ore: Math.floor(ore * pourcentageRecupere),
+          fuel: Math.floor(fuel * pourcentageRecupere),
+          energy: Math.floor(energy * pourcentageRecupere),
+        };
+      });
+    }
+  };
+  useEffect(() => {
+    getResources();
+  }, []);
+
+  return (
+    <div className="victory">
+      <div className="coupe">
+        <img src="src/Components/img/2553310_13474.png" className="coupee" />
+        <h1 className="tt">VICTOIRE</h1>
+        <img src="src/Components/img/2553310_13474.png" className="coupee" />
+        <br />
+      </div>
+      <h3 className="tt">Ressources actuelles :</h3>
+      <ul>
+        <li>
+          <p className="tt">ore : {displayOres()}</p>
+        </li>
+        <li>
+          <p className="tt">fuel : {displayFuel()}</p>
+        </li>
+        <li>
+          <p className="tt">energy : {displayEnergy()}</p>
+        </li>
+      </ul>
+      <h3 className="tt">Ressources récupérées :</h3>
+      <ul>
+        <li>
+          <p className="tt">ore : </p>
+        </li>
+        <li>
+          <p className="tt">fuel : </p>
+        </li>
+        <li>
+          <p className="tt">energy : </p>
+        </li>
+      </ul>
+    </div>
+  );
 };
 export default OtherUserProfile;
